3d09c424e1f76cb401e53db6e43289f39b8dd577
rishabh <rishabhthaney@gmail.com>
2019-03-28 20:00:18 +0530

    commented totalsupply

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,13 +1,13 @@
 pragma solidity >=0.4.22 <0.6.0;
 
-interface IERC20 {
+contract ERC20 {
     function transfer(address to, uint256 value) external returns (bool);
 
     function approve(address spender, uint256 value) external returns (bool);
 
     function transferFrom(address from, address to, uint256 value) external returns (bool);
 
-    function totalSupply() external view returns (uint256);
+//    function totalSupply() external view returns (uint256);
 
     function balanceOf(address who) external view returns (uint256);
 
