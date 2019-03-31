29a623aef7bef9fc02737fe263c4537972279997
rishabh <rishabhthaney@gmail.com>
2019-03-31 19:25:49 +0530

    atomic swap contract

---
+++ contracts/AS.sol
@@ -0,0 +1,31 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract Atomicswap {
+
+    struct Swap {
+        uint256 sTimestamp;
+        uint256 refundTime;
+        uint256 initiatorValue;
+        uint256 participantValue;
+        address initiator;
+        address participant;
+        bytes32 key;
+        bytes32 hashedkey;
+        bool emptied;
+    }
+
+    mapping(bytes32 => Swap) public swaps;
+
+    modifier isInitiator(bytes32 _hashedKey) {
+	    require(msg.sender == swaps[_hashedKey].initiator);
+	    _;
+	}
+
+	modifier isRedeemable(bytes32 _hashedkey, bytes memory _key) {
+	    require(keccak256(_key) == _hashedkey);
+		require(block.timestamp < swaps[_hashedkey].sTimestamp + swaps[_hashedkey].refundTime);
+	    require(swaps[_hashedkey].emptied == false);
+	    _;
+	}
+
+}
