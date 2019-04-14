bf34dbb0c7521a376eb2a95aa46c3d7f0d671ab4
rishabh <rishabhthaney@gmail.com>
2019-04-15 00:41:53 +0530

    testtoken mig

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,7 @@
 var AtomicSwap = artifacts.require("./AtomicSwap.sol");
+var testToken = artifacts.require("./testToken.sol");
 
 module.exports = function(deployer){
   deployer.deploy(AtomicSwap);
+  deployer.deploy(testToken);
 };
